89da77c375d9b12720088399cf5fb5f0368a39ef
Karoline Carvalho <ka.olicarvalho@gmail.com>
2022-06-13 14:54:32 -0300

    Exercício 4

--- src/components/exercicio4/index.jsx
+++ src/components/exercicio4/index.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
 function Exercicio4() {
-	return <h1 className='title'>Exercicio4</h1>
+  const [clickedTimes, setClickedTimes] = useState(0);
+
+  const clickHandle = () => {
+    setClickedTimes((previous) => {
+      return previous + 1;
+    });
+  };
+
+  return (
+    <div>
+      <h1 className="title">Exercicio4</h1>
+      <div className="exerciseContainer">
+        <p>Button has been clicked: {clickedTimes} times</p>
+        <button onClick={clickHandle}>Click Me</button>
+      </div>
+    </div>
+  );
 }
 
-export default Exercicio4
+export default Exercicio4;
